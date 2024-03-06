f199a3671f0c472c80120c8c4b2f0e506fa093fe
Sunggggggg <sky990319@gmail.com>
2024-03-06 20:21:40 +0900

    f

--- run_mask_mipnerf.py
+++ run_mask_mipnerf.py
@@ -141,8 +141,9 @@ def train(rank, world_size, args):
         train_images, train_poses = images[i_train].to(rank), poses[i_train].to(rank)    # [Unmasked_view]
         masked_view_poses = sampling_pose_function(mae_input-nerf_input)
         masked_view_images = torch.zeros((mae_input-nerf_input, *images.shape[1:]))
-        all_view_poses = torch.cat([train_poses, masked_view_poses], 0)
-        all_view_images = torch.cat([train_images, masked_view_images], 0)
+        all_view_poses = torch.cat([train_poses, masked_view_poses], 0)             #
+        all_view_images = torch.cat([train_images, masked_view_images], 0)          # 
+        print(all_view_images.shape, all_view_poses.shape)
 
         mae_input_images, mae_input_poses = mae_input_format(all_view_images, all_view_poses, mae_input, args.emb_type)
         mae_input_images = mae_input_images.type(torch.cuda.FloatTensor).to(rank)      # [1, 3, N, H, W]
