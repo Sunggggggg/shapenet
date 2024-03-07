24caea1adc001184a8bd7a1b2e75789514186a78
Sunggggggg <sky990319@gmail.com>
2024-03-07 12:27:05 +0900

    f

--- scripts/nerf_train.sh
+++ scripts/nerf_train.sh
@@ -3,4 +3,4 @@
 nerf_config=configs/MipNeRF/train.txt
 mae_config=configs/MAE/mae.txt
 
-python run_mask_mipnerf.py --nerf_config $nerf_config --mae_config $mae_config --i_print 1 --i_testset 1
+python run_mask_mipnerf.py --nerf_config $nerf_config --mae_config $mae_config
